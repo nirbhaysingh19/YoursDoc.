fac886597ea29a31c471b871b1db0291715c693f
manaspandey201 <168709137+manaspandey201@users.noreply.github.com>
2025-04-29 12:24:37 +0530

    Create DoctorSignin.js

---
+++ src/Components/DoctorSignin.js
@@ -0,0 +1,17 @@
+function DoctorSignin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <form>
+      <input type="email" placeholder="Email" 
+             value={email} onChange={(e) => setEmail(e.target.value)} />
+      
+      <input type="password" placeholder="Password" 
+             value={password} onChange={(e) => setPassword(e.target.value)} />
+
+      <button type="submit">Sign In</button>
+      <p>New doctor? <Link to="/doctor/signup">Sign up here</Link></p>
+    </form>
+  );
+}
